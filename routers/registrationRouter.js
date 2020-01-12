04c68d6fa67627ded1ca4c0e4b2b70d734672419
Filipov Maksim <filippov0705@gmail.com>
2020-01-12 11:01:35 +0200

    init/add app

---
+++ routers/registrationRouter.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const RegistratinController = require("../controllers/registrationController.js");
+const router = express.Router();
+ 
+router.route("/").post(RegistratinController.registration);
+ 
+module.exports = router;
