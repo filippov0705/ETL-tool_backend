407aa1d01961eee5df7ac3c2bb8739796002efe3
Filipov Maksim <filippov0705@gmail.com>
2020-01-12 11:14:29 +0200

    init/add app

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const usersFile = "mockData.json";
+const app = express();
+const procedureRouter = require("./routers/procedureRouter");
+const registrationRouter = require("./routers/registrationRouter");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(morgan('common'));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.use("/api/procedures/", procedureRouter);
+app.use("/api/registration", registrationRouter);
+
+app.listen(3001, () => console.log('Server has been started...'));
